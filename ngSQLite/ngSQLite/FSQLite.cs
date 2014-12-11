efb6e6e7236e1ce27661dc37c89f10ff54142c06
DMSys <open.dmsys@gmail.com>
2014-12-11 15:46:52 +0200

    new project for sqlite

---
+++ ngSQLite/ngSQLite/FSQLite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ngSQLite
+{
+    public partial class FSQLite : Form
+    {
+        private String _DataSource = "";
+
+        public FSQLite()
+        {
+            InitializeComponent();
+
+            _DataSource = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "TestBase.db");
+            
+        }
+
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SQLiteDatabase db = new SQLiteDatabase())
+                {
+                    db.Open();
+
+                    db.Close();
+                }
+                MessageBox.Show("Successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.GetType().ToString() + " : " + ex.Message);
+            }
+        }
+    }
+}
